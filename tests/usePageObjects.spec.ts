30111de9419fbdf68446984b544a3d667f164d8a
AlexFlorinHriscu <alexflorin.hriscu@gmail.com>
2024-12-10 00:51:19 +0200

    add config

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -35,9 +35,9 @@ test('parameterized methods @smoke', async({page}) => {
 })
 
 test.only('testing with argos ci', async({page}) => {
-    const pm = new PageManager(page)
-    await pm.navigateTo().formLayoutsPage()
+    const pm = new PageManager(page);
+    await pm.navigateTo().formLayoutsPage();
     await argosScreenshot(page, 'form layouts page');
-    await pm.navigateTo().datepickerPage()
+    await pm.navigateTo().datepickerPage();
     await argosScreenshot(page, 'datepicker page');
 })
